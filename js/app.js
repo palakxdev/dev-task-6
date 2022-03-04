3d8bb357190fb2c689b78fb2d4165413872607e8
Palak-SweetCoder <pspalak6@gmail.com>
2022-03-05 01:37:13 +0600

    everything done with my best

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const searchPhone = () => {
     const searchText = searchField.value;
     searchField.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    // console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(infos => displaySearchResult(infos.data))
@@ -22,7 +21,6 @@ const displaySearchResult = (infos) => {
     }
     else {
         infos.forEach(info => {
-            // console.log(info);
             const div = document.createElement('div');
             div.innerHTML = `
                 <div class="card h-100 rounded-3 shadow">
@@ -50,23 +48,22 @@ const showPhoneDetails = (phoneSlug) => {
 
 // display phone details
 const displayPhoneDetails = (details) => {
-    // console.log(details);
     const phoneDetails = document.getElementById('phone-details');
     const div = document.createElement('div');
     div.classList.add('card');
     phoneDetails.textContent = '';
     div.innerHTML = `
-    <img src="${details.image}" class="card-img-top w-50 p-4 text-center mx-auto" alt="...">
-        <hr class="ms-2 me-2">
-        <div class="card-body">
-            <h3 class="card-title">${details.name}</h3>
-            <p class="card-text"><small class="text-muted">Release Date: ${details.releaseDate}</small></p>
-            <p class="card-text text-primary">Display size: ${details.mainFeatures.displaySize}</p>
-            <p class="card-text text-primary">Chip set: ${details.mainFeatures.chipSet}</p>
-            <p class="card-text text-primary">Memory: ${details.mainFeatures.memory}</p>
-            <p class="card-text text-primary">Storage: ${details.mainFeatures.storage}</p>
-            <p class="card-text text-primary">Sensors: ${details.mainFeatures.sensors}</p>
-        </div>
-    `;
+        <img src="${details.image}" class="card-img-top w-50 p-4 text-center mx-auto" alt="...">
+            <hr class="ms-2 me-2">
+            <div class="card-body">
+                <h3 class="card-title">${details.name}</h3>
+                <p class="card-text"><small class="text-muted">Release Date: ${details.releaseDate}</small></p>
+                <p class="card-text text-primary">Display size: ${details.mainFeatures.displaySize}</p>
+                <p class="card-text text-primary">Chip set: ${details.mainFeatures.chipSet}</p>
+                <p class="card-text text-primary">Memory: ${details.mainFeatures.memory}</p>
+                <p class="card-text text-primary">Storage: ${details.mainFeatures.storage}</p>
+                <p class="card-text text-primary">Sensors: ${details.mainFeatures.sensors}</p>
+            </div>
+        `;
     phoneDetails.appendChild(div);
 }
